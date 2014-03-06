7e20526769cf2dd7c50005797418d8bafdf9941d
acjense2 <jensen.aj@outlook.com>
2014-03-05 18:16:52 -0600

    Made changes to be ready for Jordan to implement some methods

--- RubixCubeTest1/src/Cube.java
+++ RubixCubeTest1/src/Cube.java
@@ -1,11 +1,8 @@
 //class Cube
 /*
- * acjense2
- * 
  * using Ryan Heise's beginner method
  * http://www.ryanheise.com/cube/beginner.html
  * 
- * for any algorithm references:
  * front = green
  * back = blue
  * right = red
@@ -16,61 +13,53 @@
 
 class Cube {
 
+	//not used as of now
 	int FRONT = 0;
 	int BACK = 1;
 	int RIGHT = 2;
 	int LEFT = 3;
 	int UP = 4;
 	int DOWN = 5;
-	
-	/*
-	int GREEN = 1;
-	int BLUE = 2;
-	int RED = 3;
-	int ORANGE = 4;
-	int WHITE = 5;
-	int YELLOW = 6;
-	*/
-	
-	//get input from console for each row of face
-	public static void setFace(int side, char face[][][]) {
+
+	//6 faces, 3 rows, 3 columns
+	static char faces[][][] = new char[6][3][3];
+
+	// gets input from console for each row of face
+	public void setFace(int side) {
 
 		String str = "";
-
 		for (int row = 0; row < 3; row++) {
-			TextIO.putln("Enter row " + (row+1) + " of cube colors for face "
-					+ side + "as G,B,R,O,W, or Y");
+			TextIO.putln("Enter row " + (row + 1) + " of cube colors for face "
+					+ (side+1) + " as G,B,R,O,W, or Y\nSuch as \"GBR\"");
 			str = TextIO.getln();
 			for (int col = 0; col < 3; col++) {
-				face[side][row][col] = str.toLowerCase().charAt(col);
+				faces[side][row][col] = str.toLowerCase().charAt(col);
 			}
 		}
-
 	}
-	
-	//print the face to console
-	public static void printFace( int side,char face[][][]) {
-		
+
+	// prints the face to console
+	public void printFace(int side) {
 		String str = "";
-		for ( int row = 0; row < 3; row++){
-		for ( int col = 0; col < 3; col++){
-			str += face[side][row][col] + " ";
-		}
-		TextIO.putln(str);
-		str = "";
+		for (int row = 0; row < 3; row++) {
+			for (int col = 0; col < 3; col++) {
+				str += faces[side][row][col] + " ";
+			}
+			TextIO.putln(str);
+			str = "";
 		}
 		TextIO.putln();
-		
 	}
-	
-	
-	
-	public static void main(String[] args) {
-		
-		char faces[][][] = new char[6][3][3];
-		
-		setFace(2,faces);	
-		printFace(2,faces);
 
+	public char colorAt(int side, int row, int col) {
+		return faces[side][row][col];
 	}
+	
+	public boolean algOne(){
+		//search cube for wanted colors
+		//when colors are found, employ combinations of basic
+		//movement methods on the array faces. (still need to be written)
+		return true; //should return false if method is completed successfully, true if error
+	}
+
 }
--- RubixCubeTest1/src/solveCube.java
+++ RubixCubeTest1/src/solveCube.java
@@ -1,11 +1,26 @@
-
 public class solveCube {
-
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
+	
+		Cube Cube = new Cube();
 
+		for (int i = 0; i < 6; i++) {
+			Cube.setFace(i);
+		}
+		for (int i = 0; i < 6; i++) {
+			Cube.printFace(i);
+		}
+		
+		boolean error = false;
+		while (!error){
+			/*
+			 * call algorithm methods from class Cube here, in order.
+			 * set them equal to the variable error. have algorithm methods return true 
+			 * if there is an error (i.e. it can't find the color it is looking for)
+			 * */
+		}
+		
+		
 	}
 
 }
