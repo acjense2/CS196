0a3f9972ebfc6381b793bce6d8b0375d8b57835d
acjense2 <acjense2@siebl-0222-04.ews.illinois.edu>
2014-04-17 15:17:45 -0500

    completed ScanSecondLayer! modded solveCube with some new test cubes

--- RubixCubeTest1/src/solveCube.java
+++ RubixCubeTest1/src/solveCube.java
@@ -7,37 +7,40 @@ public class solveCube {
 
 		new Message("ready to go!");
 
-		/* CROSS PIECES */
-		char[] testCube = new char[] {
-				'Y', 'R', 'B', 'R', 'G', 'B', 'W', 'G', 'W',
-				'Y', 'W', 'G', 'O', 'R', 'W', 'R', 'R', 'O',
-				'O', 'W', 'B', 'O', 'B', 'B', 'W', 'B', 'B',
-				'R', 'O', 'G', 'R', 'O', 'G', 'W', 'O', 'R',
-				'Y', 'G', 'Y', 'G', 'W', 'B', 'R', 'W', 'O',
-				'B', 'Y', 'G', 'Y', 'Y', 'Y', 'O', 'Y', 'G'};
-		//bottomcorners//String stringCube = "YOWOGGGGGOWYRROWRYBRRBBBGBWBWBWOWOOOWBRGWROGGYYRYYYBYR";
-		//secondlayer
-		String stringCube = "BBRGGBGGGWGWRRWRRRROGRBWBBBWWWBOOOOOOWBGWROOGYYYYYYYYY";
-		testCube=stringCube.toCharArray();
+		/*
+		 * crosspieces//TEST1// char[] testCube = new char[] { 'Y', 'R', 'B',
+		 * 'R', 'G', 'B', 'W', 'G', 'W', 'Y', 'W', 'G', 'O', 'R', 'W', 'R', 'R',
+		 * 'O', 'O', 'W', 'B', 'O', 'B', 'B', 'W', 'B', 'B', 'R', 'O', 'G', 'R',
+		 * 'O', 'G', 'W', 'O', 'R', 'Y', 'G', 'Y', 'G', 'W', 'B', 'R', 'W', 'O',
+		 * 'B', 'Y', 'G', 'Y', 'Y', 'Y', 'O', 'Y', 'G'};
+		 */
+
+		// bottomcorners//TEST1//String stringCube =
+		// "YOWOGGGGGOWYRROWRYBRRBBBGBWBWBWOWOOOWBRGWROGGYYRYYYBYR";
+		// secondlayer//TEST1//String stringCube =
+		// "BBRGGBGGGWGWRRWRRRROGRBWBBBWWWBOOOOOOWBGWROOGYYYYYYYYY";
+		// secondlayer//TEST2//String stringCube =
+		// "RBRGGGGGGWWGRRRRRRWGWBBOBBBBOWBOOOOOOWOWWRBWGYYYYYYYYY";
+		String stringCube = "RBRGGGGGGWWGRRRRRRWGWBBOBBBBOWBOOOOOOWOWWRBWGYYYYYYYYY";
+		char[] testCube = stringCube.toCharArray();
 		Cube.setTo(testCube);
 		TextIO.putln(Cube.toString(true));
-		
+
 		/*
-		new Message("STEP 2: SCANNING FOR CROSS PIECES");
-		ScanCrossPieces.run();
-		new Message("STEP 2: done");
-		TextIO.putln(Cube.toString(true));
-		
-		new Message("STEP 3: SCANNING FOR BOTTOM CORNERS");
-		ScanBottomCorners.run();
-		new Message("STEP 3: done");
-		TextIO.putln(Cube.toString(true));
-		*/
+		 * new Message("STEP 2: SCANNING FOR CROSS PIECES");
+		 * ScanCrossPieces.run(); new Message("STEP 2: done");
+		 * TextIO.putln(Cube.toString(true));
+		 * 
+		 * new Message("STEP 3: SCANNING FOR BOTTOM CORNERS");
+		 * ScanBottomCorners.run(); new Message("STEP 3: done");
+		 * TextIO.putln(Cube.toString(true));
+		 */
 
 		new Message("STEP 4: SCANNING FOR SECOND LAYER");
 		ScanSecondLayer.run();
 		new Message("STEP 4; done");
 		TextIO.putln(Cube.toString(true));
+		
 	}
 
 }
