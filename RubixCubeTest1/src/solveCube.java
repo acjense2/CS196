34ca6ec18028801e48d8a13eab78a64879e81e78
acjense2 <acjense2@siebl-0222-04.ews.illinois.edu>
2014-04-17 18:25:10 -0500

    solveCube solves cube now. waiting on scan bottom corners bug to be fixed.

--- RubixCubeTest1/src/solveCube.java
+++ RubixCubeTest1/src/solveCube.java
@@ -5,38 +5,40 @@ public class solveCube {
 	 * **/
 	public static void main(String[] args) {
 
-		new Message("ready to go!");
+		new Message("Solving this cube:");
 
 		String stringCube = "GRGGGGGGGRGRRRRRRRBOBBBBBBBOBOOOOOOOWWWWWWWWWYYYYYYYYY";
 		Cube.setTo(stringCube.toCharArray());
 		TextIO.putln(Cube.toString(true));
 
-		/**
-		 * new Message("STEP 2: SCANNING FOR CROSS PIECES");
-		 * ScanCrossPieces.run(); new Message("STEP 2: done");
-		 * TextIO.putln(Cube.toString(true));
-		 * 
-		 * new Message("STEP 3: SCANNING FOR BOTTOM CORNERS");
-		 * ScanBottomCorners.run(); new Message("STEP 3: done");
-		 * TextIO.putln(Cube.toString(true));
-		 * 
-		 * new Message("STEP 4: SCANNING FOR SECOND LAYER");
-		 * ScanSecondLayer.run(); new Message("STEP 4: done");
-		 * 
-		 * new Message("STEP 5: SCANNING FOR TOP CROSSPIECES");
-		 * ScanTopCross.run(); new Message("STEP 5: DONE");
-		 * 
-		 * new Message("STEP 6: SCANNING FOR TOP CORNERS");
-		 * ScanTopCornersUp.run(); new Message("STEP 6: DONE");
-		 */
-		
+		new Message("STEP 2: SCANNING FOR CROSS PIECES");
+		ScanCrossPieces.run();
+		new Message("STEP 2: done");
+
+		new Message("STEP 3: SCANNING FOR BOTTOM CORNERS");
+		ScanBottomCorners.run();
+		new Message("STEP 3: done");
+
+		new Message("STEP 4: SCANNING FOR SECOND LAYER");
+		ScanSecondLayer.run();
+		new Message("STEP 4: done");
+
+		new Message("STEP 5: SCANNING FOR TOP CROSSPIECES");
+		ScanTopCross.run();
+		new Message("STEP 5: DONE");
+
+		new Message("STEP 6: SCANNING FOR TOP CORNERS");
+		ScanTopCornersUp.run();
+		new Message("STEP 6: DONE");
+
 		new Message("STEP 7: SCANNING SIDES OF TOP CORNERS");
-		ScanTopCornerSides.run(); new Message("STEP 7: DONE");
-		
+		ScanTopCornerSides.run();
+		new Message("STEP 7: DONE");
+
 		new Message("STEP 8: SCANNING FOR TOP EDGES");
 		ScanTopEdges.run();
 		new Message("STEP 8: DONE");
-		
+
 		TextIO.putln(Cube.toString(true));
 		if (Cube.equals(stringCube))
 			new Message("CUBE SOLVED!");
